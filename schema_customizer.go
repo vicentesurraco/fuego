abf71f204f7b1627e9cd2efd0b4ebee09850e495
vicentesurraco <134657715+vicentesurraco@users.noreply.github.com>
2026-01-19 13:29:44 -0800

    Add debug logging for enum extraction

--- schema_customizer.go
+++ schema_customizer.go
@@ -72,6 +72,7 @@ func parseValidate(tag reflect.StructTag, schema *openapi3.Schema) {
 					schema.Enum = append(schema.Enum, v)
 				}
 			}
+			slog.Info("[ENUM DEBUG] Parsed oneof enum values", "enumValues", schema.Enum)
 		}
 	}
 }
@@ -165,6 +166,7 @@ func parseEnum(tag reflect.StructTag, schema *openapi3.Schema) {
 			schema.Enum = append(schema.Enum, v)
 		}
 	}
+	slog.Info("[ENUM DEBUG] parseEnum found explicit enum tag", "enumValues", schema.Enum)
 }
 
 // SchemaCustomizer parses struct tags and modifies the schema using kin-openapi3gen's
@@ -223,8 +225,10 @@ func parseEnumValuer(t reflect.Type, schema *openapi3.Schema) {
 	if reflect.PointerTo(enumType).Implements(enumValuerType) {
 		instance := reflect.New(enumType).Interface().(EnumValuer)
 		schema.Enum = instance.EnumValues()
+		slog.Info("[ENUM DEBUG] parseEnumValuer found EnumValuer (pointer)", "typeName", enumType.Name(), "enumValues", schema.Enum)
 	} else if enumType.Implements(enumValuerType) {
 		instance := reflect.Zero(enumType).Interface().(EnumValuer)
 		schema.Enum = instance.EnumValues()
+		slog.Info("[ENUM DEBUG] parseEnumValuer found EnumValuer (value)", "typeName", enumType.Name(), "enumValues", schema.Enum)
 	}
 }
